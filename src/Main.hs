99fe5ba293ec3c3f451b4a8f0db494eaafdf110d
Matija Herceg <herc.rules@gmail.com>
2015-01-20 11:23:27 +0100

    restructuring

--- src/Main.hs
+++ src/Main.hs
@@ -5,22 +5,16 @@ import Data.List
 import System.Environment
 import System.Directory
 import Hash
+--Entry point for the program. 
+--Checks if an argument is given and if so checks if it is a valid input
 main :: IO ()
 main = do
 	a <- getArgs
-	exists <- doesFileExist (a !! 0)
-	if exists then if check (a!!0) then Hash.runScript (a!!0) else
-		putStrLn "Invalid file, should be a .hash script"
-		else
-			putStrLn ("File " ++ (a !! 0) ++ " doesn't exist")
-
-
-	{-if null a then Hash.runInteractive else 
-		if (check (a!!0)) then Hash.runScript (a!!0) else do
-			if exists then 
-				putStrLn "Invalid file, should be a .hash script"
-				else
-					putStrLn ("File " ++ (a !! 0) ++ " doesn't exist")
-					-}
+	if null a then Hash.runInteractive else do
+		exists <- doesFileExist (a !! 0)
+		if exists then if check (a!!0) then Hash.runScript (a!!0) else
+			putStrLn "Invalid file, should be a .hash script"
+			else
+				putStrLn ("File " ++ (a !! 0) ++ " doesn't exist")
 	where
 		check x = ".hash" `isSuffixOf` x
